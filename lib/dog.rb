b0b6cc7c165e53d5b71f68c0bf63632daa81c316
Walker Le <walker.le88@gmail.com>
2023-01-30 11:36:56 -0700

    commit tree 30a9afe1ffb4d356825e6a220965e33716462290

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,23 @@
 class Dog
+  def name= name
+    @name = name
+  end
 
+  def name
+    @name
+  end
+
+  def breed= breed
+    @breed = breed
+  end
+
+  def breed
+    @breed
+  end
 end
+
+lassie = Dog.new
+lassie.name = "Lassie"
+lassie.breed = "Collie"
+puts lassie.name
+puts lassie.breed
--- lib/person.rb
+++ lib/person.rb
@@ -1,3 +1,23 @@
 class Person
+  def name= name
+    @name = name
+  end
 
+  def name
+    @name
+  end
+
+  def job= job
+    @job = job
+  end
+
+  def job
+    @job
+  end
 end
+
+# walker = Person.new
+# walker.name = "Walker"
+# walker.job = "Student"
+# puts walker.name
+# puts walker.job
